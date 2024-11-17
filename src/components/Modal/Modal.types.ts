b4326601a4fbc3c705757d8cb5584c6e5e41f376
loanTranduy <tdl.tranduy@gmail.com>
2024-11-17 10:34:44 +0100

    Modal.tsx updated into a compound component

--- src/components/Modal/Modal.types.ts
+++ src/components/Modal/Modal.types.ts
@@ -1,11 +1,23 @@
-import { ReactNode } from 'react';
+import { FC, ReactElement, ReactNode } from 'react';
+import { ButtonProps } from '@/components/ui/button';
 
 export type TModal = {
   children: ReactNode;
   onOpenChange?: (open: boolean) => void;
   open?: boolean;
+};
+
+export type TTrigger = {
+  children: ReactElement<ButtonProps>;
+};
+
+export type TContent = {
   title: string;
-  description: string;
-  actionAriaLabel: string;
-  actionLabel: string;
+  description?: string;
+  children: ReactNode;
+};
+
+export type SubComponentsProps = {
+  Content: FC<TContent>;
+  Trigger: FC<TTrigger>;
 };
