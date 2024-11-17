868bccbc7b2a3a51a9d17814c799a691bdf8999a
loanTranduy <tdl.tranduy@gmail.com>
2024-11-17 15:54:29 +0100

    Table responsiveness fixed + MobileWarning for mobile created + global UI improved part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import CreateEntity from '@/components/CreateEntity/CreateEntity';
 import TableEntities from '@/components/TableEntities/TableEntities';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import MobileWarning from '@/components/MobileWarning/MobileWarning';
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-10">
-      <CreateEntity />
-      <TableEntities />
-    </main>
+    <>
+      <div className="sm:block hidden">
+        <Card className="bg-card rounded-xl border-none overflow-hidden">
+          <CardHeader className="flex flex-row items-center justify-between p-8">
+            <CardTitle>Your Entities</CardTitle>
+            <CreateEntity />
+          </CardHeader>
+          <TableEntities />
+        </Card>
+      </div>
+      <MobileWarning />
+    </>
   );
 }
--- src/components/TableEntities/TableEntities.tsx
+++ src/components/TableEntities/TableEntities.tsx
@@ -7,31 +7,41 @@ import { useTableEntities } from '@/components/TableEntities/TableEntities.hook'
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import './TableEntities.scss';
+import { Loader2 } from 'lucide-react';
 
 const TableEntities = () => {
   const { loading, error, data } = useQuery(GET_ENTITIES);
-  const { columnDefs, defaultColDef, onGridReady } = useTableEntities();
+  const { columnDefs, defaultColDef } = useTableEntities();
   const entities = data?.getEntities || [];
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <div className="flex items-center justify-center mb-8">
+        <Loader2 className="animate-spin text-blue-500 w-8 h-8" />
+      </div>
+    );
+
   if (error) return <p>Error: {error.message}</p>;
 
   const rowHeight = 60;
-  const paginationPageSize = 20;
+  const paginationPageSize = 15;
   const hasPagination = entities.length > paginationPageSize;
   return (
-    <div className="ag-theme-alpine grid-container">
-      <AgGridReact
-        onGridReady={onGridReady}
-        rowHeight={rowHeight}
-        className="ag-grid"
-        columnDefs={columnDefs}
-        rowData={entities}
-        defaultColDef={defaultColDef}
-        pagination={hasPagination}
-        paginationPageSize={paginationPageSize}
-      />
-    </div>
+    <>
+      {entities.length > 0 && (
+        <div className="ag-theme-alpine grid-container">
+          <AgGridReact
+            rowHeight={rowHeight}
+            className="ag-grid"
+            columnDefs={columnDefs}
+            rowData={entities}
+            defaultColDef={defaultColDef}
+            pagination={hasPagination}
+            paginationPageSize={paginationPageSize}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
